5ce0722772e5917b17b3198ed7b7c8eff9ea0765
Taavi Kivisik <taavi.kivisik@gmail.com>
2019-02-26 18:48:44 +0100

    tdd str2num done and mark task in progress

--- main.go
+++ main.go
@@ -2,45 +2,78 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-var mapping = map[string]int{
-	"n": 0,
-	"l": 1,
-	"k": 2,
-	"g": 2,
-	"m": 3,
-	"t": 4,
-	"v": 5,
-	"w": 5,
-	"f": 5,
-	"p": 6,
-	"b": 6,
-	"s": 7,
-	"š": 7,
-	"z": 7,
-	"ž": 7,
-	"r": 8,
-	"j": 9,
-	"d": 9,
-	"h": 9,
+var mapping = map[string]string{
+	"n": "0",
+	"l": "1",
+	"k": "2",
+	"g": "2",
+	"m": "3",
+	"t": "4",
+	"v": "5",
+	"w": "5",
+	"f": "5",
+	"p": "6",
+	"b": "6",
+	"s": "7",
+	"š": "7",
+	"z": "7",
+	"ž": "7",
+	"r": "8",
+	"j": "9",
+	"d": "9",
+	"h": "9",
 }
 
 // String2Num converts strings to numbers
 func String2Num(str string) string {
-	//for i := 0; i < len(str); i++ {
-	//	str[i] in mapping
+	output := strings.Builder{}
+	for _, ch := range str {
+		ch := strings.ToLower(string(ch))
+		fmt.Fprint(&output, mapping[ch])
+	}
+	return output.String()
+}
 
-	//}
-	return str
+func MarkTask(challenge string, response string, assessment map[string]string) {
+	correct := String2Num(challenge)
+	// all correct
+	if correct == response {
+		for _, ch := range challenge {
+			number := mapping[string(ch)]
+			if number == "" {
+				continue
+			}
+			assessment[number] += "+"
+		}
+		return
+	}
+
+	// var err error
+	// cReader := strings.NewReader(String2Num(challenge))
+	// rReader := strings.NewReader(response)
+	// for err == nil {
+	// 	chCorr, _, err := cReader.ReadRune()
+	// 	if err == io.EOF {
+	// 		err = nil
+	// 		for chResp := rReader.ReadRune(); err == nil
+
+	// 	}
+	// 	if err != nil {
+
+	// 	}
+
+	// 	correct
+	// }
+	//return map[string]string{}
 }
 
 func main() {
-
-	var result string
-	var allikas = "kala"
-	result = String2Num(allikas)
-
-	fmt.Println(result)
-	fmt.Println(mapping)
+	MarkTask("appi", "66", map[string]string{})
+	// rand.Seed(time.Now().UTC().UnixNano())
+	// for i := 0; i < 25; i++ {
+	// 	fmt.Println(rand.Intn(10))
+	// }
 }
---
+++ main_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestString2Num(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    string
+		expected string
+	}{
+		{
+			desc:     "empty input",
+			input:    "",
+			expected: "",
+		},
+		{
+			desc:     "letter not in map",
+			input:    "a",
+			expected: "",
+		},
+		{
+			desc:     "number as input",
+			input:    "1",
+			expected: "",
+		},
+		{
+			desc:     "correct input lower case",
+			input:    "luuk",
+			expected: "12",
+		},
+		{
+			desc:     "correct input upper-lower case",
+			input:    "LuUk",
+			expected: "12",
+		},
+		{
+			desc:     "letter order",
+			input:    "kuul",
+			expected: "21",
+		},
+		{
+			desc:     "correct input",
+			input:    "amet",
+			expected: "34",
+		},
+		{
+			desc:     "correct input",
+			input:    "vaip",
+			expected: "56",
+		},
+		{
+			desc:     "correct input",
+			input:    "saar",
+			expected: "78",
+		},
+		{
+			desc:     "correct input",
+			input:    "hani",
+			expected: "90",
+		},
+		{
+			desc:     "correct input double same letter",
+			input:    "lukk",
+			expected: "122",
+		},
+		{
+			desc:     "sentence as input",
+			input:    "hello world!",
+			expected: "9115819",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if output := String2Num(tC.input); output != tC.expected {
+				t.Fatalf("expected f(%s) = %s, got: %s", tC.input, tC.expected, output)
+			}
+		})
+	}
+}
+
+func TestMarkTask(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		challenge string
+		response  string
+		expected  map[string]string
+	}{
+		{
+			desc:      "all correct",
+			challenge: "lukk",
+			response:  "122",
+			expected:  map[string]string{"1": "+", "2": "++"},
+		},
+		{
+			desc:      "all wrong",
+			challenge: "lukk",
+			response:  "000",
+			expected:  map[string]string{"1": "-", "2": "--"},
+		},
+		{
+			desc:      "partially wrong",
+			challenge: "lukk",
+			response:  "132",
+			expected:  map[string]string{"1": "+", "2": "-+"},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			assessment := map[string]string{}
+			if MarkTask(tC.challenge, tC.response, assessment); fmt.Sprint(assessment) != fmt.Sprint(tC.expected) {
+				t.Fatalf("expected f(%s, %s) = %s, got: %s", tC.challenge, tC.response, tC.expected, assessment)
+			}
+		})
+	}
+}
