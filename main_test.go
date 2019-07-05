e53def291729515018e54e9750bbff159625d3c5
Taavi Kivisik <taavi.kivisik@gmail.com>
2019-07-06 01:44:56 +0200

    more compact test case representation

--- main_test.go
+++ main_test.go
@@ -7,75 +7,29 @@ import (
 
 func TestString2Num(t *testing.T) {
 	testCases := []struct {
-		desc     string
-		input    string
-		expected string
+		desc  string
+		input string
+		want  string
 	}{
-		{
-			desc:     "empty input",
-			input:    "",
-			expected: "",
-		},
-		{
-			desc:     "letter not in map",
-			input:    "a",
-			expected: "",
-		},
-		{
-			desc:     "number as input",
-			input:    "1",
-			expected: "",
-		},
-		{
-			desc:     "correct input lower case",
-			input:    "luuk",
-			expected: "12",
-		},
-		{
-			desc:     "correct input upper-lower case",
-			input:    "LuUk",
-			expected: "12",
-		},
-		{
-			desc:     "letter order",
-			input:    "kuul",
-			expected: "21",
-		},
-		{
-			desc:     "correct input",
-			input:    "amet",
-			expected: "34",
-		},
-		{
-			desc:     "correct input",
-			input:    "vaip",
-			expected: "56",
-		},
-		{
-			desc:     "correct input",
-			input:    "saar",
-			expected: "78",
-		},
-		{
-			desc:     "correct input",
-			input:    "hani",
-			expected: "90",
-		},
-		{
-			desc:     "correct input double same letter",
-			input:    "lukk",
-			expected: "122",
-		},
-		{
-			desc:     "sentence as input",
-			input:    "hello world!",
-			expected: "9115819",
-		},
+		{desc: "empty input", input: "", want: ""},
+		{desc: "letter not in map", input: "a", want: ""},
+		{desc: "number as input", input: "1", want: ""},
+		{desc: "correct input lower case", input: "luuk", want: "12"},
+		{desc: "correct input mixed case", input: "LuUk", want: "12"},
+		{desc: "letter order 21", input: "kuul", want: "21"},
+		{desc: "correct input 34", input: "amet", want: "34"},
+		{desc: "correct input 56", input: "vaip", want: "56"},
+		{desc: "correct input 78", input: "saar", want: "78"},
+		{desc: "correct input 90", input: "hani", want: "90"},
+		{desc: "correct input double same letter", input: "lukk", want: "122"},
+		{desc: "sentence as input", input: "hello world!", want: "9115819"},
+		{desc: "common symbols success", input: "!@#$s%^&*m()_+s", want: "737"},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			if output := String2Num(tC.input); output != tC.expected {
-				t.Fatalf("expected f(%s) = %s, got: %s", tC.input, tC.expected, output)
+			if output := String2Num(tC.input); output != tC.want {
+				t.Errorf("String2Num(%q) = %q, want %q", tC.input, output, tC.want)
+				// t.Fatalf("want f(%s) = %s, got: %s", tC.input, tC.want, output)
 			}
 		})
 	}
@@ -86,44 +40,20 @@ func TestMarkTask(t *testing.T) {
 		desc      string
 		challenge string
 		response  string
-		expected  map[string]string
+		want      map[string]string
 	}{
-		{
-			desc:      "all correct",
-			challenge: "lukk",
-			response:  "122",
-			expected:  map[string]string{"1": "+", "2": "++"},
-		},
-		{
-			desc:      "all wrong",
-			challenge: "lukk",
-			response:  "000",
-			expected:  map[string]string{"1": "-", "2": "--", "0": "---"},
-		},
-		{
-			desc:      "partially wrong",
-			challenge: "lukk",
-			response:  "132",
-			expected:  map[string]string{"1": "+", "2": "-+", "3": "-"},
-		},
-		{
-			desc:      "no response",
-			challenge: "lukk",
-			response:  "",
-			expected:  map[string]string{"1": "-", "2": "--"},
-		},
-		{
-			desc:      "too long",
-			challenge: "luu",
-			response:  "123456",
-			expected:  map[string]string{"1": "+", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"},
-		},
+		{desc: "all correct", challenge: "lukk", response: "122", want: map[string]string{"1": "+", "2": "++"}},
+		{desc: "all wrong", challenge: "lukk", response: "000", want: map[string]string{"1": "-", "2": "--", "0": "---"}},
+		{desc: "partially wrong", challenge: "lukk", response: "132", want: map[string]string{"1": "+", "2": "-+", "3": "-"}},
+		{desc: "no response", challenge: "lukk", response: "", want: map[string]string{"1": "-", "2": "--"}},
+		{desc: "too long", challenge: "luu", response: "123456", want: map[string]string{"1": "+", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"}},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 			assessment := map[string]string{}
-			if MarkTask(tC.challenge, tC.response, assessment); fmt.Sprint(assessment) != fmt.Sprint(tC.expected) {
-				t.Fatalf("expected f(%s, %s) = %s, got: %s", tC.challenge, tC.response, tC.expected, assessment)
+			if MarkTask(tC.challenge, tC.response, assessment); fmt.Sprint(assessment) != fmt.Sprint(tC.want) {
+				// t.Errorf("MarkTask(%q) = %q, want %q", tC.challenge, assessment, tC.want)
+				t.Errorf("MarkTask(%q, %q) = %v, want: %v", tC.challenge, tC.response, assessment, tC.want)
 			}
 		})
 	}
